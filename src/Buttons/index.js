6a1eb508f60564d7b31bac62ddb2fa23b97fec52
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-06-07 12:19:03 +0200

    Button converted to styled components

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,10 +1,10 @@
-import "./style.css";
+import { Flex, FlexButton } from "./styled";
 
 const Buttons = ({hideAllTasks, hideTasks, doneAllTasks, showOrHide}) => (
-  <div className="buttonsFlex">
-    <button onClick={hideAllTasks} className="buttonsFlex__button">{hideTasks ? "Pokaż" : "Ukryj"} ukończone</button>
-    <button onClick={doneAllTasks} className="buttonsFlex__button">{showOrHide ? "Odznacz" : "Ukończ"} wszystkie</button>
-  </div>
+  <Flex>
+    <FlexButton onClick={hideAllTasks}>{hideTasks ? "Pokaż" : "Ukryj"} ukończone</FlexButton>
+    <FlexButton onClick={doneAllTasks}>{showOrHide ? "Odznacz" : "Ukończ"} wszystkie</FlexButton>
+  </Flex>
 );
 
 export default Buttons;
---
+++ src/Buttons/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+export const Flex = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    @media (max-width: 968px) {
+        flex-direction: column;
+}
+`;
+
+export const FlexButton = styled.div`
+    max-width: 100px;
+    margin: 10px;
+
+    &:hover {
+    color: rgb(158, 158, 158);
+    cursor: pointer;
+    }
+`;
+
