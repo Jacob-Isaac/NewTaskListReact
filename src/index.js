795fd01dd4450da298aa01b1b804f10dc859b4b9
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-07-06 17:33:18 +0200

    globalStyle created instead of index.css

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.css';
+import {GlobalStyle} from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <GlobalStyle/>
     <App />
     </Provider>
   </React.StrictMode>,
