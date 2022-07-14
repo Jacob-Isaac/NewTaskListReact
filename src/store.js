1e05e7896f3c6f239e7828b6dcf8ceaafa0a6abd
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-07-15 00:06:15 +0200

    saga middleware added to store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import createSagaMiddleware from "@redux-saga/core";
+// import reducer from "./reducer";
+// import saga from "./saga";
 import taskListReducer from "./features/taskList/tasksSlice";
+import {watchFetchExampleTasks} from "./features/taskList/tasksSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
   reducer: {
     taskList: taskListReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(watchFetchExampleTasks);
+
+export default store;
