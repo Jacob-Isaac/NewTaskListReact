67db3ca6715e6e1aab586fd769417e194f2879c4
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-07-20 21:44:04 +0200

    app structure changed for hashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import React from "react";
-import TaskList from "./features/taskList/TaskList";
+import TaskListPage from "./features/taskList/TaskListPage";
+import TaskPage from "./features/taskList/TaskPage";
+import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
 
-const App = () => <TaskList />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact activeClassName="active" to="/zadania">Zadania</NavLink>
+        </li>
+        <li>
+          <NavLink exact activeClassName="active" to="/author">Autor</NavLink>
+        </li>
+      </ul>
+      <Switch>
+      <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TaskListPage />
+        </Route>
+        <Route path="/author">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+       <Redirect to="/zadania" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
 
 export default App;
+
+//11:48 / 14.05
