f25a708541cdaa1bc9b3003d8410050c811d0aef
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-25 21:23:19 +0200

    document format

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,9 +1,6 @@
-import {all} from "redux-saga/effects";
+import { all } from "redux-saga/effects";
 import { tasksSaga } from "./features/taskList/tasksSaga";
 
-
 export default function* rootSaga() {
-    yield all([
-        tasksSaga(),
-    ]);
+  yield all([tasksSaga()]);
 }
